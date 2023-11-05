6dca08777875fd5bc1a7f6807ba917a748d9b8ec
Adnurhytia <adindanurhayati30@gmail.com>
2023-11-05 13:45:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dumdumteaapp/HalamanHome.kt
@@ -0,0 +1,10 @@
+package com.example.dumdumteaapp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HalamanHome(
+    onNextButtonClicked: () -> Unit
+){
+
+}
---
+++ app/src/main/java/com/example/dumdumteaapp/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.dumdumteaapp.data
+
+import com.example.dumdumteaapp.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lychee
+    )
+}
