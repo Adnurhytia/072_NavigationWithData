e6344079fc51c8c3f42cce585c05e4ba107c344b
Adnurhytia <adindanurhayati30@gmail.com>
2023-11-14 22:54:09 +0700

    Menambahkan outlinedTextField utk mengisi form

--- app/src/main/java/com/example/dumdumteaapp/HalamanLogin.kt
+++ app/src/main/java/com/example/dumdumteaapp/HalamanLogin.kt
@@ -1,11 +1,22 @@
 package com.example.dumdumteaapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanLogin(
     onSubmitButtonClick: (MutableList<String>) -> Unit
@@ -20,4 +31,28 @@ fun HalamanLogin(
         mutableStateOf("")
     }
     var txtListData: MutableList<String> = mutableListOf(txtNama,txtAlamat,txtTelpon)
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ){
+        OutlinedTextField(value =txtNama , onValueChange ={
+            txtNama = it
+        }, label = {
+            Text(text = "Nama")
+        })
+        OutlinedTextField(value =txtAlamat , onValueChange ={
+            txtAlamat = it
+        }, label = {
+            Text(text = "Alamat")
+        })
+        OutlinedTextField(value =txtTelpon , onValueChange ={
+            txtTelpon = it
+        }, label = {
+            Text(text = "Telpon")
+        })
+    }
 }
