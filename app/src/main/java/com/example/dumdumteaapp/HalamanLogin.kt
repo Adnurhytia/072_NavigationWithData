7a5ce7d59852b0ad568b80cf9ec26ca15b35ceca
Adnurhytia <adindanurhayati30@gmail.com>
2023-11-14 22:50:20 +0700

    Menambahkan variabel utk menyimpan data

--- app/src/main/java/com/example/dumdumteaapp/HalamanLogin.kt
+++ app/src/main/java/com/example/dumdumteaapp/HalamanLogin.kt
@@ -1,7 +1,23 @@
 package com.example.dumdumteaapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@Composable
 fun HalamanLogin(
     onSubmitButtonClick: (MutableList<String>) -> Unit
 ){
-
+    var txtNama by rememberSaveable {
+        mutableStateOf("")
+    }
+    var txtAlamat by rememberSaveable {
+        mutableStateOf("")
+    }
+    var txtTelpon by rememberSaveable {
+        mutableStateOf("")
+    }
+    var txtListData: MutableList<String> = mutableListOf(txtNama,txtAlamat,txtTelpon)
 }
